87c6ab1eb4fd11faeab2cee9867d1d6d89198566
JoeQuigley1 <joeyjtq@hotmail.com>
2023-03-05 15:27:54 +0000

    fix secret key error

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -31,7 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x#o92x7x)h*t*=#+d-6oh1p6$6se&s!^8$c0p!w_m#dp+k+4%q'
+SECRET_KEY = os.getenv('SECRET_KEY')
+# 'django-insecure-x#o92x7x)h*t*=#+d-6oh1p6$6se&s!^8$c0p!w_m#dp+k+4%q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
